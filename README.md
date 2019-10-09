d22af7798a8a58d704238d6ae13955f174ad1bfd
nycrera <alperenyurdakul7@gmail.com>
2019-10-09 22:31:18 +0300

    Basically Implement

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # GitHubInviteBot
-An api for adding people to organization. (Warning don't use this completely public)
+An api for adding people to organization.
+Warning! Do not use this completely public as if this tool is not intended for such use.
+And can lead to unwanted results. Maybe we can secure this then we can talk this matter again ;)
+But for now this project has no plans of supporting this.
+
+npm install
+node index.js
---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const app = express()
+const port = 3001
+app.use(express.urlencoded());
+const Octokit = require("@octokit/rest");
+const Token = "yourAuthToken"; //Check https://github.com/settings/tokens
+const Organization = "organizationName" // Example https://github.com/Cool-Organization -> Cool-Organization
+
+app.post('/', function (req, res){
+    var type = req.body.type;
+    if(type === "mail"){
+        addUserbyMail(req.body.mail);
+        res.send("ok");
+        return;
+    }
+    if(type === "id"){
+        addUserbyID(req.body.id);
+        res.send("ok");
+    }
+    res.status(500);
+    res.end();
+});
+
+app.listen(port, () => console.log(`Github Organization Invite Bot listening on port ${port}!`))
+
+function addUserbyID(id){
+    const octokit = new Octokit({
+        auth() {
+          return Token;
+        }
+       });
+       octokit.orgs.createInvitation({
+        org: Organization,
+        invitee_id: id,
+        role: "direct_member"
+      });
+
+}
+
+function addUserbyMail(mail){
+    const octokit = new Octokit({
+        auth() {
+          return Token;
+        }
+       });
+       octokit.orgs.createInvitation({
+        org: Organization,
+        email: mail,
+        role: "direct_member"
+      });
+
+}
